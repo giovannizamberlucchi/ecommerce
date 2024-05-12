d40b50c6b1b0667954098f2e18979dbee137629c
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-12 20:29:52 +0300

    feat: qraphql categories  parent, breadcrumbs

--- src/app/_graphql/categories.ts
+++ src/app/_graphql/categories.ts
@@ -5,11 +5,44 @@ export const PRODUCT_CATEGORIES = `categories {
     id
     label
   }
-}`
+}`;
 
-export const CATEGORIES = `
+export const CATEGORIES = `#graphql
   query Categories {
     Categories(limit: 300) {
+      docs {
+        id
+        title
+        slug
+        breadcrumbs {
+          id
+          label
+          url
+        }
+        media {
+          alt
+          width
+          height
+          url
+        }
+        parent {
+          id
+          title
+          media {
+            alt
+            width
+            height
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const CATEGORY = `#graphql
+  query Category($slug: String, $breadcrumbUrl: String) {
+    Categories(where: { slug: { equals: $slug }, breadcrumbs__url: { equals: $breadcrumbUrl } }, limit: 1) {
       docs {
         id
         title
@@ -19,7 +52,17 @@ export const CATEGORIES = `
           height
           url
         }
+        parent {
+          id
+          title
+          media {
+            alt
+            width
+            height
+            url
+          }
+        }
       }
     }
   }
-`
+`;
