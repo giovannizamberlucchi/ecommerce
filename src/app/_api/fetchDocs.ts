4cb9d5fd354f147c5170357f83f39b566db8d6e8
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-12 20:19:19 +0300

    feat: fetchDocs variables, return generic PaginatedDocs

--- src/app/_api/fetchDocs.ts
+++ src/app/_api/fetchDocs.ts
@@ -1,12 +1,13 @@
-import type { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
+import type { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
-import type { Config } from '../../payload/payload-types'
-import { ORDERS } from '../_graphql/orders'
-import { PAGES } from '../_graphql/pages'
-import { PRODUCTS } from '../_graphql/products'
-import { GRAPHQL_API_URL } from './shared'
-import { payloadToken } from './token'
-import { CATEGORIES } from '../_graphql/categories'
+import type { Config } from '../../payload/payload-types';
+import { ORDERS } from '../_graphql/orders';
+import { PAGES } from '../_graphql/pages';
+import { PRODUCTS } from '../_graphql/products';
+import { GRAPHQL_API_URL } from './shared';
+import { payloadToken } from './token';
+import { CATEGORIES } from '../_graphql/categories';
+import { PaginatedDocs } from 'payload/database';
 
 const queryMap = {
   pages: {
@@ -25,22 +26,23 @@ const queryMap = {
     query: CATEGORIES,
     key: 'Categories',
   },
-}
+};
 
 export const fetchDocs = async <T>(
   collection: keyof Config['collections'],
   draft?: boolean,
-): Promise<T[]> => {
-  if (!queryMap[collection]) throw new Error(`Collection ${collection} not found`)
+  variables?: Record<string, unknown>,
+): Promise<PaginatedDocs<T>> => {
+  if (!queryMap[collection]) throw new Error(`Collection ${collection} not found`);
 
-  let token: RequestCookie | undefined
+  let token: RequestCookie | undefined;
 
   if (draft) {
-    const { cookies } = await import('next/headers')
-    token = cookies().get(payloadToken)
+    const { cookies } = await import('next/headers');
+    token = cookies().get(payloadToken);
   }
 
-  const docs: T[] = await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
+  const docs: PaginatedDocs<T> = await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -50,14 +52,15 @@ export const fetchDocs = async <T>(
     next: { tags: [collection] },
     body: JSON.stringify({
       query: queryMap[collection].query,
+      variables,
     }),
   })
-    ?.then(res => res.json())
-    ?.then(res => {
-      if (res.errors) throw new Error(res?.errors?.[0]?.message ?? 'Error fetching docs')
+    ?.then((res) => res.json())
+    ?.then((res) => {
+      if (res.errors) throw new Error(res?.errors?.[0]?.message ?? 'Error fetching docs');
 
-      return res?.data?.[queryMap[collection].key]?.docs
-    })
+      return res?.data?.[queryMap[collection].key];
+    });
 
-  return docs
-}
+  return docs;
+};
