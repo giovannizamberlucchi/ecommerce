6bc716eae766d1a3ecba11586c3f48eab700d779
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-16 18:24:50 +0300

    feat: page categories filter for attributes

--- src/app/(pages)/categories/[...slug]/page.tsx
+++ src/app/(pages)/categories/[...slug]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { draftMode } from 'next/headers';
 
 import classes from './index.module.scss';
-import { Category, Product } from '../../../../payload/payload-types';
+import { Attribute, Category, Page, Product } from '../../../../payload/payload-types';
 import { fetchDoc } from '../../../_api/fetchDoc';
 import { fetchDocs } from '../../../_api/fetchDocs';
 import { Gutter } from '../../../_components/Gutter';
@@ -12,7 +12,12 @@ import { notFound } from 'next/navigation';
 import { getLastFromArray, getPathFromSlugArr } from '../../../_api/utils';
 import { getChildrenIds } from '../../_utils';
 import { PaginatedDocs } from 'payload/database';
-import { CollectionArchive } from '../../../_components/CollectionArchive';
+import { CollectionProducts } from '../../../_components/CollectionProducts';
+import { AttributesPillsList } from '../../../_components/AttributesPillsList';
+import { AttributesFilter } from '../../../_components/AttributesFilter';
+import { AttributesOverlay } from '../../../_components/AttributesOverlay';
+import { Metadata } from 'next';
+import { generateMeta } from '../../../_utilities/generateMeta';
 
 type CategoriesProps = {
   params: {
@@ -25,13 +30,29 @@ type CategoriesProps = {
 
 export const dynamic = 'force-dynamic';
 
-const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchParams: { page = '1' } }) => {
+const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchParams }) => {
   const { isEnabled: isDraftMode } = draftMode();
+  const { page = '1' } = searchParams;
 
   let categories: Category[] | null = null;
   let category: Category | null = null;
   let productsData: PaginatedDocs<Product> | null = null;
-  const limit = 10;
+  let attributesForProducts: Attribute[] | null = null;
+  let AllProductsAttributes: Product[] | null = null;
+
+  const attributesEntries = Object.entries(searchParams)
+    .map<[string, string[]]>(([key, value]) => [key, Array.isArray(value) ? value : [value]])
+    .filter(([key, value]) => key !== 'page');
+  let attributes: Record<string, string[]> = attributesEntries.reduce(
+    (acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    },
+    {} as Record<string, string[]>,
+  );
+
+  const limit = 12;
 
   try {
     category = await fetchDoc<Category>({
@@ -43,6 +64,20 @@ const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchP
 
     categories = (await fetchDocs<Category>('categories')).docs;
 
+    attributesForProducts =
+      (
+        await fetchDocs<Attribute>('attributes', isDraftMode, {
+          filterAttributesByAttribute: Object.keys(attributes),
+        })
+      ).docs || [];
+
+    attributesForProducts.map((attr) => {
+      if (attributes[attr.attribute] !== undefined) {
+        attributes[attr.id] = attributes[attr.attribute];
+        delete attributes[attr.attribute];
+      }
+    });
+
     const subcategoriesIds = getChildrenIds(category.id, categories);
 
     productsData = await fetchDocs<Product>('products', isDraftMode, {
@@ -50,9 +85,32 @@ const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchP
       page: Number(page),
       limit,
     });
+
+    AllProductsAttributes = (
+      await fetchDocs<Product>('products-attributes', isDraftMode, {
+        filterCategoriesByIds: subcategoriesIds,
+      })
+    ).docs;
   } catch (error) {
     console.log(error);
   }
+
+  let productsAttributesObject: { [key: string]: string[] } = {};
+
+  (AllProductsAttributes || []).map((product) =>
+    product.attributes.map((attr) => {
+      if (attr.type === null && typeof attr.type === 'string' && attr.type === undefined) return;
+      const type = attr.type as Attribute;
+      if (
+        productsAttributesObject[type.attribute] !== undefined &&
+        !productsAttributesObject[type.attribute].includes(attr.value)
+      )
+        productsAttributesObject[type.attribute] = [...productsAttributesObject[type.attribute], attr.value];
+      else productsAttributesObject[type.attribute] = [attr.value];
+    }),
+  );
+
+  const productsAttributesEntries = Object.entries(productsAttributesObject);
 
   if (!category) return notFound();
   if (Number(page) > productsData?.totalPages) return notFound();
@@ -62,8 +120,24 @@ const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchP
   return (
     <div className={classes.container}>
       <Gutter className={classes.products}>
-        <Filters category={category} categories={categories} subcategories={subcategories} slug={slug} />
-        <CollectionArchive page={Number(page)} productsData={productsData} limit={limit} />
+        <AttributesPillsList attributes={attributesEntries} className={classes['attributes-pill--desktop']} />
+
+        <div>
+          <Filters category={category} categories={categories} subcategories={subcategories} slug={slug} />
+          <AttributesFilter
+            attributes={productsAttributesEntries}
+            className={classes['container-attributes-list--desktop']}
+          />
+        </div>
+
+        <div>
+          <AttributesPillsList attributes={attributesEntries} className={classes['attributes-pill--mobile']} />
+          <AttributesOverlay
+            attributes={productsAttributesEntries}
+            className={classes['container-attributes-list--mobile']}
+          />
+          <CollectionProducts page={Number(page)} productsData={productsData} limit={limit} />
+        </div>
       </Gutter>
       <HR />
     </div>
@@ -71,3 +145,21 @@ const Categories: React.FC<CategoriesProps> = async ({ params: { slug }, searchP
 };
 
 export default Categories;
+
+export async function generateMetadata({ params: { slug } }): Promise<Metadata> {
+  const { isEnabled: isDraftMode } = draftMode();
+
+  let category: Category | null = null;
+
+  try {
+    category = await fetchDoc<Category>({
+      collection: 'categories',
+      slug: getLastFromArray(slug),
+      draft: isDraftMode,
+    });
+  } catch (error) {}
+
+  if (!category) return;
+
+  return generateMeta({ doc: { title: `Catégorie ${category?.title}` || `Catégorie ${getLastFromArray(slug)}` } });
+}
