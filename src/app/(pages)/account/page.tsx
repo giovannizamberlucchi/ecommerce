7f01ec112c6eb6431cce2a4a39131cb1df32adec
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-18 21:13:13 +0300

    feat: page account status subscription

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -5,12 +5,17 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph';
 import AccountForm from './AccountForm';
 
 import classes from './index.module.scss';
+import { getMeUser } from '../../_utilities/getMeUser';
+import { isActiveSubscription } from '../../_utilities/isActiveSubscription';
 
 export default async function Account() {
+  const { user } = await getMeUser();
+  const isActiveSubs = await isActiveSubscription(user);
   return (
     <div>
       <h5 className={classes.personalInfo}>Informations personelles</h5>
       <AccountForm />
+      <h5>État de l'abonnement:{isActiveSubs ? 'actif' : 'inactif'}</h5>
     </div>
   );
 }
