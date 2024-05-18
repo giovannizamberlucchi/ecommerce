24086656f80c16313e468221c4ba92ce17d25b99
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-18 21:07:23 +0300

    feat: utilities isActiveSubscription

---
+++ src/app/_utilities/isActiveSubscription.ts
@@ -0,0 +1,21 @@
+import { User } from '../../payload/payload-types';
+import Stripe from 'stripe';
+
+export const isActiveSubscription = async (userData: User): Promise<boolean> => {
+  const apiKey = `${process.env.STRIPE_SECRET_KEY}`;
+  const stripe = new Stripe(apiKey);
+
+  if (!userData.stripeCustomerID && userData.stripeCustomerID === undefined) return false;
+
+  const customerResponse = await stripe.customers.retrieve(userData.stripeCustomerID, { expand: ['subscriptions'] });
+
+  if (customerResponse.deleted !== undefined) return false;
+  const customer = customerResponse as Stripe.Customer;
+  if (!customer.subscriptions && customer.subscriptions.data.length === 0) return false;
+
+  for (const subscription of customer.subscriptions.data) {
+    if (subscription.status === 'active') return true;
+  }
+
+  return false;
+};
