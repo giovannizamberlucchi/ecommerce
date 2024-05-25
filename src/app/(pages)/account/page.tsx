37093ce391bfbbdbdf469e861541ae61efab85af
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-25 20:26:27 +0300

    feat: page account referral link

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -11,11 +11,21 @@ import { isActiveSubscription } from '../../_utilities/isActiveSubscription';
 export default async function Account() {
   const { user } = await getMeUser();
   const isActiveSubs = await isActiveSubscription(user);
+
+  const refUrl =
+    user?.referralCode !== null
+      ? `${process.env.NEXT_PUBLIC_SERVER_URL}/create-account?referral=${user?.referralCode}`
+      : '';
+
   return (
     <div>
       <h5 className={classes.personalInfo}>Informations personelles</h5>
       <AccountForm />
       <h5>État de l'abonnement:{isActiveSubs ? 'actif' : 'inactif'}</h5>
+
+      <p className={classes.referral}>
+        Votre lien de parrainage : <strong>{refUrl}</strong>
+      </p>
     </div>
   );
 }
