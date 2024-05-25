d4d22015915aa39c3e44933f9f9370beadd9ae46
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-25 20:36:55 +0300

    fix: collection Users endpoint re-subscribe del comments

--- src/payload/collections/Users/endpoints/re-subscribe.ts
+++ src/payload/collections/Users/endpoints/re-subscribe.ts
@@ -2,15 +2,6 @@ import type { PayloadHandler } from 'payload/config';
 import type { PayloadRequest } from 'payload/types';
 import Stripe from 'stripe';
 
-const logs = process.env.LOGS_STRIPE_PROXY === '1';
-
-// use this handler to interact with a Stripe customer associated with any given user
-// does so in secure way that does not leak or expose any cross-customer data
-// pass the proper method and body to this endpoint to interact with the Stripe API
-// available methods:
-// GET /api/users/:id/customer
-// POST /api/users/:id/customer
-// body: { customer: Stripe.CustomerUpdateParams }
 export const reSubscribe: PayloadHandler = async (req: PayloadRequest, res) => {
   if (!req.body.userId) return res.status(400).json({ error: 'Missing userId' });
 
