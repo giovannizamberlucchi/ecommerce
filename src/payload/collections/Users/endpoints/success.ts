67130b4078f7fc6af4e31803042a80bc3e57c1a5
Maksym Volkov <maksym.volkov@zapal.tech>
2024-05-25 20:22:31 +0300

    feat: collection Users endpoints signUp, success referral code

--- src/payload/collections/Users/endpoints/success.ts
+++ src/payload/collections/Users/endpoints/success.ts
@@ -1,8 +1,17 @@
 import type { PayloadHandler } from 'payload/config';
 import type { PayloadRequest } from 'payload/types';
 import Stripe from 'stripe';
+import { fetchSettings } from '../../../../app/_api/fetchGlobals';
+import { User } from '../../../payload-types';
 
 const logs = process.env.LOGS_STRIPE_PROXY === '1';
+
+const templateEmail = ({ user, referral }: { user: User; referral: User }) => {
+  return `Bonjour, RESOVALIE team !
+  
+  ${user.name} vient de s'inscrire sur la plateforme Resovalie Achats avec un code de parrainage utilisateur ${referral.name}.
+  `;
+};
 
 // use this handler to interact with a Stripe customer associated with any given user
 // does so in secure way that does not leak or expose any cross-customer data
@@ -30,8 +39,43 @@ export const success: PayloadHandler = async (req: PayloadRequest, res) => {
   };
 
   try {
-    const userData = await req.payload.create({ collection: 'users', data });
+    if (req.query.referral && req.query.referral !== null) {
+      const referrer = await req.payload.find({
+        collection: 'users',
+        where: {
+          referralCode: { equals: req.query.referral as string },
+        },
+        limit: 1,
+      });
 
+      if (referrer) {
+        const user = await req.payload.create({ collection: 'users', data });
+        await req.payload.update({
+          collection: 'users',
+          id: referrer.docs[0].id,
+          data: {
+            referrals: [...referrer.docs[0].referrals, user.id],
+          },
+        });
+
+        const settings = await fetchSettings();
+
+        const emailData = {
+          to: settings.teamEmail,
+          subject: 'Système de référence',
+          from: settings.teamEmail,
+          text: templateEmail({ user, referral: referrer.docs[0] }),
+        };
+
+        await req.payload.sendEmail(emailData);
+      }
+    } else {
+      await req.payload.create({ collection: 'users', data });
+    }
     return res.redirect('/login');
-  } catch (error) {}
+  } catch (error) {
+    return res.redirect(
+      `/login?error=${encodeURIComponent("Une erreur s'est produite lors de la création du compte")}`,
+    );
+  }
 };
