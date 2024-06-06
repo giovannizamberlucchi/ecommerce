32106d677a7ed013c05068718afbe5a54b69febf
Maksym Volkov <maksym.volkov@zapal.tech>
2024-06-06 16:31:36 +0300

    feat: collection Users hook createReferralCode prefix RESOV

--- src/payload/collections/Users/hooks/createReferralCode.ts
+++ src/payload/collections/Users/hooks/createReferralCode.ts
@@ -2,7 +2,7 @@ import { FieldHook } from 'payload/types';
 import { v4 } from 'uuid';
 
 export const createReferralCode: FieldHook = async ({ operation, value }) => {
-  if (operation === 'create' && !value) value = v4().split('-')[0];
+  if (operation === 'create' && !value) value = `RESOV${v4().split('-')[0]}`;
 
   return value;
 };
