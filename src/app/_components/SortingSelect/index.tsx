f48db3e2bd0900a00af0b3c87ca67bc4d8cdec4b
Maksym Volkov <maksym.volkov@zapal.tech>
2024-06-10 18:03:11 +0300

    fix: component SortingSelect refactoring

--- src/app/_components/SortingSelect/index.tsx
+++ src/app/_components/SortingSelect/index.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Select } from '../Select';
-import Link from 'next/link';
 import classes from './index.module.scss';
 import { useMemo } from 'react';
 
@@ -25,11 +24,7 @@ const options = [
   },
 ];
 
-type SortingSelectProps = {
-  className: string;
-};
-
-export const SortingSelect: React.FC<SortingSelectProps> = ({ className }) => {
+export const SortingSelect: React.FC = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const currentOption = useMemo(() => options.find(({ value }) => value === searchParams.get('sort')), [searchParams]);
